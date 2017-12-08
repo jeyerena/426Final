c9087a37c1f2b78e48d166186ac20330ec80706a
jeyerena <jeduardoyerena@gmail.com>
2017-12-07 22:42:24 -0500

    ship serializer works; now need to link it to the server and link this page to the board creater

--- BattleshipServer/BattleshipServer/game.js
+++ BattleshipServer/BattleshipServer/game.js
@@ -1,18 +1,30 @@
 $(document).ready(function () {
+	var num;
 
     document.getElementById('build').onclick = function () {
-		var num = parseInt(document.getElementById('boardSize').value, 10);
-        tableCreate(num, "left");
-        tableCreate(num, "right");
+		num = parseInt(document.getElementById('boardSize').value, 10);
+		tableCreate(num, "top");
+		tableCreate(num, "bottom");
+		
         var div = document.getElementById('bCreation');
         div.style.visibility = "hidden";
         div.style.display = "none";
-        var table = document.getElementById('table-middle');
+        var table = document.getElementById('table-top');
         var cells = table.getElementsByTagName('td');
         for (var i = 0; i < cells.length; i++) {
             var cell = cells[i];
             cell.onclick = shot;
-        }
+		}
+		//have it receive the normal player data from the server
+		//or pullit from previous page
+		var tmp = []
+		tmp.push({'x':0, 'y':0, 'length':2, 'isVertical':true})
+		tmp.push({'x':2, 'y':0, 'length':4, 'isVertical':false})			
+		tmp.push({'x':2, 'y':4, 'length':2, 'isVertical':false})	
+		tmp.push({'x':4, 'y':6, 'length':1, 'isVertical':false})	
+		tmp.push({'x':7, 'y':5, 'length':3, 'isVertical':true})	
+		tmp.push({'x':9, 'y':9, 'length':1, 'isVertical':true})			
+		placeShips('bottom', JSON.stringify(tmp));
 	};
 
 	function tableCreate(num, position) {
@@ -27,29 +39,46 @@ $(document).ready(function () {
                 var td = tr.insertCell();
                 td.style.width = (150/num + num-1) + 'px';
                 td.style.height = (150/num + num-1) + 'px';
-                td.id = i + ',' + j;
+                td.id = position + '-' + i + ',' + j;
                // td.innerHTML = i + ',' + j;
                 td.style.border = '1px solid black';
             }
         }
 		body.appendChild(tbl);
-		placeShips('a');
 	}
 	
-	function placeShips(ships){
-		var p = {}
-		p['x'] = 0;
-		p['y'] = 0;
-		p['length'] = 2;
-		p['width'] = 1;
-		p['isVertical'] = false;
-		var shi = JSON.stringify(p);
-		var q = JSON.parse(shi);
-		ships = JSON.stringify(ships);		
+	function placeShips(position, shipJSON){
+		ships = JSON.parse(shipJSON);
+		//check if it's array
+		for (var i = 0; i < ships.length; i++){
+			var ship = ships[i];
+			if (ship.isVertical){
+				if (ship.x >= 0 && ship.x < num && ship.y >= 0){
+					for (var j = 0; j < ship.length; j++){
+						if (ship.y + j < num){
+							var cellID = position + '-' + (ship.y + j) + ',' + ship.x;									
+							console.log(cellID);
+							document.getElementById(cellID).classList.toggle('w3-blue');
+						}
+					}
+				}
+			}
+			else{
+				if (ship.y >= 0 && ship.y < num && ship.x >= 0){
+					for (var j = 0; j < ship.length; j++){
+						if (ship.x + j < num){
+							var cellID = position + '-' + ship.y + ',' + (ship.x + j);	
+							console.log(cellID);
+							document.getElementById(cellID).classList.toggle('w3-blue');
+						}
+					}
+				}
+			}
+		}
 	}
 
     function shot() {
-		this.classList.toggleClass('w3-red')
+		this.classList.toggle('w3-red');
         var cell = {};
         cell['x'] = this.id.split(',')[0];
         cell['y'] = this.id.split(',')[1];
@@ -57,7 +86,11 @@ $(document).ready(function () {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.open("POST", "/json-handler");
         xmlhttp.setRequestHeader("Content-Type", "application/json");
-        xmlhttp.send(cell);
+		xmlhttp.send(cell);
+		//display message based on server response
+		var message = document.createElement('p');
+		message.innerHTML = 'Hit!';
+		document.getElementById('events').appendChild(message);
   //      this.className = '';
     }
 });
