f97269a914bfb9fe080552d5f1ce9a9c42dc8776
Peter <peterzlnt@gmail.com>
2017-12-14 16:06:52 -0500

    stuff

--- BattleShipServer/Models/MatchDBContext.cs
+++ BattleShipServer/Models/MatchDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,7 @@ namespace BattleShipServer.Models
 			this.ConnectionString = connectionString;
 			MySqlConnection conn = GetConnection();
 			conn.Open();
-			MySqlCommand cmd = new MySqlCommand("drop table Matches;", conn);
+			MySqlCommand cmd = new MySqlCommand("drop table if exists Matches;", conn);
 			cmd.ExecuteScalar();
 			cmd = new MySqlCommand("create table Matches (" +
 										"matchId integer primary key auto increment," +
@@ -66,5 +67,18 @@ namespace BattleShipServer.Models
 		//basically Match.shipConfig is an int array of ship lengths
 		//if 2 ship configs match, the 2 serialized jsons of the ship configs should also exactly match
 		//have fun!
+		public List<Match> FindMatches(GameConfig config)
+		{
+			List<Match> list = new List<Match>();
+			int xSize = config.xSize;
+			int ySize = config.ySize;
+			string shipConfig = JsonConvert.SerializeObject(new ShipConfig(config));
+
+			MySqlConnection conn = GetConnection();
+			conn.Open();
+			MySqlCommand cmd = new MySqlCommand();
+			conn.Close();
+			return list;
+		}
     }
 }
